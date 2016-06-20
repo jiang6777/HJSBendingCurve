90bcc4ac1d4317d2758587785aa0c6489e52f0ca
jiang6777 <jiang677@yeah.net>
2016-06-20 13:32:46 +0800

    提交整个项目

--- HJSBendingCurve/ViewController.m
+++ HJSBendingCurve/ViewController.m
@@ -7,21 +7,52 @@
 //
 
 #import "ViewController.h"
+#import "HJSBendingView.h"
+#import "Masonry.h"
+#define SYRealValue(value) ((value)/375.0f*[UIScreen mainScreen].bounds.size.width)
 
 @interface ViewController ()
-
+@property(nonatomic,strong)UILabel *outputLabel;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self configView];
+}
+
+- (void)configView
+{
+    __weak ViewController *this = self;
+    HJSBendingView *bendingView = [[HJSBendingView alloc] initWithFrame:CGRectMake(0, 0, SYRealValue(110), SYRealValue(110))];
+    bendingView.outputValueBlock = ^(HJSBendingView *bendingView,NSString *value) {
+        this.outputLabel.text = value;
+    };
+    [self.view addSubview:bendingView];
+    
+    [bendingView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+        make.centerY.mas_equalTo(self.view.mas_centerY);
+        make.width.mas_equalTo(SYRealValue(110));
+        make.height.mas_equalTo(SYRealValue(110));
+    }];
+    
+    self.outputLabel = [[UILabel alloc] init];
+    self.outputLabel.textAlignment = NSTextAlignmentCenter;
+    self.outputLabel.textColor = [UIColor blackColor];
+    self.outputLabel.text = @"0";
+    [self.view addSubview:self.outputLabel];
+    [self.outputLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(bendingView.mas_bottom).offset(SYRealValue(20));
+        make.centerX.mas_equalTo(bendingView.mas_centerX);
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+ 
 }
 
 @end
