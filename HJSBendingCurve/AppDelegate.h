3e909dbfe0a876cbedbaef07777fb9a3f29d0d4a
jiang6777 <jiang677@yeah.net>
2016-06-20 09:43:24 +0800

    Initial Commit

---
+++ HJSBendingCurve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJSBendingCurve
+//
+//  Created by hejiangshan on 16/6/20.
+//  Copyright © 2016年 飞兽科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJSBendingCurve/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJSBendingCurve
+//
+//  Created by hejiangshan on 16/6/20.
+//  Copyright © 2016年 飞兽科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
